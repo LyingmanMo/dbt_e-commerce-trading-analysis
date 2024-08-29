5089fc513c33119e4ff32e5708f70922e2d670a9
Xinqiang LIU <moyuan20@hotmail.com>
2024-08-29 09:49:39 +0000

    structuring staging files and CTE Groupings and Cosmetic Cleanups

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,15 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+with 
+source as (
+    select * from {{ source('jaffle_shop', 'customers') }}
+),
 
-from raw.jaffle_shop.orders
+transformed as (
+    select 
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from source
+)
+
+select * from transformed
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,18 @@
-select 
-    id as payment_id, 
-    orderid as order_id, 
-    paymentmethod as payment_method, 
-    status, 
-    amount/100 as amount, 
-    created as created_id
-    
-from raw.stripe.payment
+with 
+
+source as (
+    select * from {{ source('stripe', 'payment') }}
+),
+
+transformed as (
+    select 
+        id as payment_id, 
+        orderid as order_id, 
+        paymentmethod as payment_method, 
+        status, 
+        amount/100 as amount, 
+        created as created_id
+    from source
+)
+
+select * from transformed
