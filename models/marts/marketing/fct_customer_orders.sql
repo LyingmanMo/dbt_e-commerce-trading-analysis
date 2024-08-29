d000f799f6e7e869206f469b03528e94e18bd4d0
Xinqiang LIU <moyuan20@hotmail.com>
2024-08-29 10:49:37 +0000

    Moving logic to intermediate models

--- models/marts/marketing/fct_customer_orders.sql
+++ models/marts/marketing/fct_customer_orders.sql
@@ -6,32 +6,7 @@ customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}
-    where order_status not in ('pending')
-),
-
-payments as (
-    select * from {{ ref('stg_stripe__payments') }}
-    where payment_status != 'fail'
-),
-
-order_totals as (
-    select 
-        order_id,
-        payment_status,
-        sum(payment_amount) as order_value_dollars
-    from payments
-    group by 1, 2
-),
-
-order_values_joined as (
-    select 
-        orders.*,
-        order_totals.payment_status,
-        order_totals.order_value_dollars
-    from orders
-    left join order_totals
-        on orders.order_id = order_totals.order_id
+    select * from {{ ref('int_orders') }}
 ),
 
 -- logical CTEs
@@ -45,15 +20,15 @@ customer_order_history as (
         min(valid_order_date) as first_non_returned_order_date,
         max(valid_order_date) as most_recent_non_returned_order_date,
         coalesce(max(user_order_seq),0) as order_count,
-        coalesce(count(case when order_values_joined.order_status != 'returned' then 1 end),0) as non_returned_order_count,
-        sum(case when valid_order_date is not null then order_values_joined.order_value_dollars else 0 end) as total_lifetime_value,
-        sum(case when valid_order_date is not null then order_values_joined.order_value_dollars else 0 end)/nullif(count(case when valid_order_date is not null then 1 end),0) as avg_non_returned_order_value,
-        array_agg(distinct order_values_joined.order_id) as order_ids
+        coalesce(count(case when orders.order_status != 'returned' then 1 end),0) as non_returned_order_count,
+        sum(case when valid_order_date is not null then orders.order_value_dollars else 0 end) as total_lifetime_value,
+        sum(case when valid_order_date is not null then orders.order_value_dollars else 0 end)/nullif(count(case when valid_order_date is not null then 1 end),0) as avg_non_returned_order_value,
+        array_agg(distinct orders.order_id) as order_ids
 
-    from order_values_joined
+    from orders
 
     join customers
-    on order_values_joined.customer_id = customers.customer_id
+    on orders.customer_id = customers.customer_id
 
     group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
 ),
@@ -71,13 +46,13 @@ final as (
         order_value_dollars,
         order_status,
         payment_status
-    from order_values_joined
+    from orders
 
     join customers
-    on order_values_joined.customer_id = customers.customer_id
+    on orders.customer_id = customers.customer_id
 
     join customer_order_history
-    on order_values_joined.customer_id = customer_order_history.customer_id
+    on orders.customer_id = customer_order_history.customer_id
 
 )
 
