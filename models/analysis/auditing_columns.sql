4da17aa4179e5ea36087d74d698b2d4283a60c7e
Xinqiang LIU <moyuan20@hotmail.com>
2024-08-29 17:30:11 +0000

    auditing files renamed

--- models/analysis/auditing_columns.sql
+++ models/analysis/auditing_columns.sql
@@ -1,4 +1,4 @@
-{% set old_etl_relation = adapter.get_relation(
+{% set old_relation = adapter.get_relation(
     database = "analytics",
     schema = "dbt_xliu",
     identifier = "customer_orders") 
@@ -7,9 +7,8 @@
 {% set dbt_relation=ref('fct_customer_orders') %}
 
 {% if execute %}
-{{ audit_helper.compare_relations(
-    a_relation=old_etl_relation,
-    b_relation=dbt_relation,
-    primary_key="order_id"
+{{ audit_helper.compare_relation_columns(
+    a_relation=old_relation,
+    b_relation=dbt_relation
 ) }}
 {% endif %}
